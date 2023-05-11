399ac61c2fca4d1ca1c52a0c8b385abb87574583
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-11 20:47:59 +0200

    Add endpoint for getting a project by its public ID

--- src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectsController.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectsController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/projects")
@@ -21,6 +23,16 @@ public class ProjectsController {
     @GetMapping
     public ResponseEntity<List<Project>> getAllProjects() {
         return new ResponseEntity<>(projectService.getAllProjects(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectPublicId}")
+    public ResponseEntity<Project> getProjectByProjectPublicId(@PathVariable("projectPublicId") int projectPublicId) {
+        Optional<Project> project = projectService.getProjectByProjectPublicId(projectPublicId);
+        if (project.isPresent()) {
+            return ResponseEntity.ok(project.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
