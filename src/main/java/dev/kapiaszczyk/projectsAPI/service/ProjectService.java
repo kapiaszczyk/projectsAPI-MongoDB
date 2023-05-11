2260d26fcdf26bccbbaa3fac3a7794f2456cf3db
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-11 20:50:53 +0200

    Add getProjectByProjectPublicId method for getting a project by its projectPublicId field

--- src/main/java/dev/kapiaszczyk/projectsAPI/service/ProjectService.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/service/ProjectService.java
@@ -1,11 +1,12 @@
 package dev.kapiaszczyk.projectsAPI.service;
 
 import dev.kapiaszczyk.projectsAPI.document.Project;
-import dev.kapiaszczyk.projectsAPI.respository.ProjectRepository;
+import dev.kapiaszczyk.projectsAPI.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -16,5 +17,11 @@ public class ProjectService {
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
     }
+
+    public Optional<Project> getProjectByProjectPublicId(int projectPublicId) {
+        return projectRepository.findByProjectPublicId(projectPublicId);
+    }
+
+
 
 }
