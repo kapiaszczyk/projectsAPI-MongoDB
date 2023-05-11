144ec97d9c305cc87f9e6b2fbe9c5dc66803332c
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-11 18:21:43 +0200

    Add ProjectController class

--- src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectsController.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectsController.java
@@ -1,9 +1,27 @@
 package dev.kapiaszczyk.projectsAPI.controller;
 
+import dev.kapiaszczyk.projectsAPI.document.Project;
+import dev.kapiaszczyk.projectsAPI.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/projects")
 public class ProjectsController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllProjects() {
+        return new ResponseEntity<>(projectService.getAllProjects(), HttpStatus.OK);
+    }
+
 
 }
