e8bcabb979a8129e72844ab5b17adbcb0d490439
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-11 17:56:47 +0200

    Initial commit

---
+++ src/main/java/dev/kapiaszczyk/projectsAPI/ProjectsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.kapiaszczyk.projectsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kapiaszczyk/projectsAPI/ProjectsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kapiaszczyk.projectsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
