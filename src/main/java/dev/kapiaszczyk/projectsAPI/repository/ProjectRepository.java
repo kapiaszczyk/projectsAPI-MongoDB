d3654d19ca217b5eacf0e9f8cb4bd9788c6193fe
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-11 20:49:41 +0200

    Add method for finding a project by its public ID

--- src/main/java/dev/kapiaszczyk/projectsAPI/repository/ProjectRepository.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/repository/ProjectRepository.java
@@ -1,9 +1,15 @@
 package dev.kapiaszczyk.projectsAPI.repository;
 
 import dev.kapiaszczyk.projectsAPI.document.Project;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ProjectRepository extends MongoRepository<Project, String> {
+public interface ProjectRepository extends MongoRepository<Project, ObjectId> {
+
+        Optional<Project> findByProjectPublicId(int projectPublicId);
+
 }
