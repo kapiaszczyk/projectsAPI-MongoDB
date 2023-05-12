5595b9c931a57776abfbf730969e7b182a2a90e6
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-12 07:43:46 +0200

    Add findProjectNameLike query to retrieve a list of Project objects based on their name

--- src/main/java/dev/kapiaszczyk/projectsAPI/repository/ProjectRepository.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/repository/ProjectRepository.java
@@ -5,11 +5,14 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends MongoRepository<Project, ObjectId> {
 
         Optional<Project> findByProjectPublicId(int projectPublicId);
+
+        List<Project> findByProjectNameLike(String projectName);
 
 }
