fb2c8339c3f746ffd35245718cbe1ff7567daeea
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-12 08:39:14 +0200

    Add setters, getters and constructor to the Technology class

--- src/main/java/dev/kapiaszczyk/projectsAPI/document/Technology.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/document/Technology.java
@@ -9,6 +9,49 @@ public class Technology {
 
     @Id
     private ObjectId technologyId;
+    private int technologyPublicId;
     private String technologyName;
     private String technologyDescription;
+
+    public ObjectId getTechnologyId() {
+        return technologyId;
+    }
+
+    public void setTechnologyId(ObjectId technologyId) {
+        this.technologyId = technologyId;
+    }
+
+    public int getTechnologyPublicId() {
+        return technologyPublicId;
+    }
+
+    public void setTechnologyPublicId(int technologyPublicId) {
+        this.technologyPublicId = technologyPublicId;
+    }
+
+    public String getTechnologyName() {
+        return technologyName;
+    }
+
+    public void setTechnologyName(String technologyName) {
+        this.technologyName = technologyName;
+    }
+
+    public String getTechnologyDescription() {
+        return technologyDescription;
+    }
+
+    public void setTechnologyDescription(String technologyDescription) {
+        this.technologyDescription = technologyDescription;
+    }
+
+    public Technology(ObjectId technologyId, int technologyPublicId, String technologyName, String technologyDescription) {
+        this.technologyId = technologyId;
+        this.technologyPublicId = technologyPublicId;
+        this.technologyName = technologyName;
+        this.technologyDescription = technologyDescription;
+    }
+
+    public Technology() {
+    }
 }
