06298647e5919d5aecdc8aa81407aae27a7162c8
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-12 16:13:45 +0200

    Add endpoint for adding Project objects

--- src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectController.java
+++ src/main/java/dev/kapiaszczyk/projectsAPI/controller/ProjectController.java
@@ -5,10 +5,7 @@ import dev.kapiaszczyk.projectsAPI.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -53,6 +50,11 @@ public class ProjectController {
         } else {
             return ResponseEntity.ok(projects);
         }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Project> addProject(@RequestBody Project project) {
+        return new ResponseEntity<>(projectService.addProject(project), HttpStatus.CREATED);
     }
 
 }
